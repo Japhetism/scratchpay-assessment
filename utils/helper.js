4851aad6f9cd76f783eacca49699f6736b6193f8
Babatunde Ojo <babatundeojo@MacBook-Pro.local>
2021-07-11 08:39:30 +0100

    implement case insentive to clinic filter by name and state

--- utils/helper.js
+++ utils/helper.js
@@ -16,8 +16,8 @@ exports.paginate = (doc, req) => {
 exports.filterBy = (doc, req) => {
     const {query: { name, state, availabilityFrom, availabilityTo } } = req;
     const filteredDoc = doc
-        .filter(item => name ? item.name === name : item)
-        .filter(item => state ? item.state === state : item)
+        .filter(item => name ? item.name.toLowerCase() === name.toLowerCase() : item)
+        .filter(item => state ? item.state.toLowerCase() === state.toLowerCase() : item)
         .filter(item => availabilityFrom && availabilityTo ? item.availability.from >= availabilityFrom && item.availability.to <= availabilityTo : item)
     
         return this.paginate(filteredDoc, req)
