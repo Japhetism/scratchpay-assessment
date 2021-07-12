24cbf48e87b9ad6dc96c00f3766f22e7517d3b0f
Babatunde Ojo <babatundeojo@MacBook-Pro.local>
2021-07-12 08:22:33 +0100

    update validation rule

--- utils/validator.js
+++ utils/validator.js
@@ -2,13 +2,13 @@ const Joi = require('joi');
 
 exports.validateClinicQueryParams = (req) => {
     const schema = Joi.object().keys({
-        name: Joi.string().min(2),
+        name: Joi.string().alphanum().min(2),
         state: Joi.string().min(2),
-        avalabilityFrom: Joi.string(),
-        avalabilityTo: Joi.string(),
+        availabilityFrom: Joi.string().pattern(new RegExp('^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$')),
+        availabilityTo: Joi.string().pattern(new RegExp('^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$')),
         currentPage: Joi.number().integer().min(1),
         pageSize: Joi.number().integer().min(1)
-    })
+    }).with('availabilityTo', ['availabilityFrom']);
     
     const result = schema.validate(req.query)
 
